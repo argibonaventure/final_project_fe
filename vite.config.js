b8f4fff12254481feabe52c2f5452b1c27f8a9b5
argibonaventure <127969187+argibonaventure@users.noreply.github.com>
2024-08-09 20:51:39 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  base: '/final_project_fe/',  // Replace 'repo-name' with the name of your GitHub repository
+  // other configurations...
+}
+
